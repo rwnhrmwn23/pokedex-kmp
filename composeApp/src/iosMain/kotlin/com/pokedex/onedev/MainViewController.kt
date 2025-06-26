c2bab06ace394d920a8b09f8fa89c773c2e71f0c
Irwan Hermawan <rwnhermawan@gmail.com>
2025-06-26 07:04:23 +0700

    add initKoin in MainViewController ios

--- composeApp/src/iosMain/kotlin/com/pokedex/onedev/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/pokedex/onedev/MainViewController.kt
@@ -1,5 +1,9 @@
 package com.pokedex.onedev
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.pokedex.onedev.presentation.core.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    initKoin()
+    App()
+}
